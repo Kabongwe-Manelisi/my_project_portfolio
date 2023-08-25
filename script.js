587be14c7797902912bc9176bb8b370e973ee491
Kabongwe-Manelisi <118818827+Kabongwe-Manelisi@users.noreply.github.com>
2023-08-25 12:55:09 +0200

    nav modification 3 without styling

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function showElementDesign() {
-  document.getElementById('show-area').innerHTML = '<div class="show-area"><span><div class="item__container"><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/details/zYJeWLz"data-hover="Dropdown Menu Button "class="item"><img src="showcase-items/dropdown_menu.jpg" alt="Dropdown menu button"class="img"></a></div><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/details/vYzGKZo"data-hover="Space Odyssey Background"class="item"><img src="showcase-items/space_odyssey_background.jpg" alt="Dropdown menu button"class="img"></a></div><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/pen/XWydVRY"data-hover="Scroll onclick"class="item"><img src="showcase-items/scroll_onclick_button.jpg" alt="scroll-onclick"class="img"></a></div></div></span></div>';
+//navigation bar button functions
+function showCodingNav() {
+  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><button onclick="showPens(document.getElementById(\'show-area\'))">Codepens</button><button onclick="showLayouts(document.getElementById(\'show-area\'))">Layouts</button></div>'
 }
-function showWebPages() {
-  document.getElementById('show-area').innerHTML = '<div class="show-area"><span><div class="item__container"><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/details/XWPwRwo"data-hover="Portfolio Layout"class="item"><img src="showcase-items/webpage_layout1.0.jpg" alt="webpage_layout1.0"class="img"></a></div></div></span></div>'
+function showGraphicsNav() {
+  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><button onclick="showGraphics(document.getElementById(\'show-area\'))">Graphics</button></div>'
 }
-function showPictures() {
-  document.getElementById('show-area').innerHTML = '<div class="show-area"><span><div class="item__container"><div class="item_img_container"></div></div></span></div>';
+function showProgramsNav() {
+  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><button onclick="showSoftware(document.getElementById(\'show-area\'))">Software</button><button onclick="showApps(document.getElementById(\'show-area\'))">Apps</button></div>'
 }
-function showVideos() {
-  document.getElementById('show-area').innerHTML = '<div class="show-area"><span><div class="item__container"><div class="item_img_container"></div></div></span></div>'
+//drop down menue content button functions
+function showPens() {
+  document.getElementById('show-area').innerHTML = '<div class="show-area"><div class="item__container"><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/details/zYJeWLz"data-hover="Dropdown Menu Button "class="item"><img src="showcase-items/dropdown_menu.jpg" alt="Dropdown menu button"class="img"></a></div><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/details/vYzGKZo"data-hover="Space Odyssey Background"class="item"><img src="showcase-items/space_odyssey_background.jpg" alt="Dropdown menu button"class="img"></a></div><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/pen/XWydVRY"data-hover="Scroll onclick"class="item"><img src="showcase-items/scroll_onclick_button.jpg" alt="scroll-onclick"class="img"></a></div></div></div>';
 }
-function showPhotos() {
-  document.getElementById('show-area').innerHTML = '<div class="show-area"><span><div class="item__container"><div class="item_img_container"></div></div></span></div>'
+function showLayouts() {
+  document.getElementById('show-area').innerHTML = '<div class="show-area"><div class="item__container"><div class="item_img_container"><a href="https://codepen.io/DemonNoSh0kan/details/XWPwRwo"data-hover="Portfolio Layout"class="item"><img src="showcase-items/webpage_layout1.0.jpg" alt="webpage_layout1.0"class="img"></a></div></div></span></div>'
+}
+function showGraphics() {
+  document.getElementById('show-area').innerHTML = '<div class="show-area"><div class="item__container"><div class="item_img_container"></div></div></div>';
+}
+function showSoftware() {
+  document.getElementById('show-area').innerHTML = '<div class="show-area"><div class="item__container"><div class="item_img_container"></div></div></div>'
+}
+function showApps() {
+  document.getElementById('show-area').innerHTML = '<div class="show-area"><div class="item__container"><div class="item_img_container"></div></div></div>'
 }
