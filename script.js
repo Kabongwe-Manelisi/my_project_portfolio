deaa5447d619367b88c52ce5ed94bda9e95dce39
Kabongwe-Manelisi <118818827+Kabongwe-Manelisi@users.noreply.github.com>
2023-09-14 15:37:03 +0200

    graphics drop menu update

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 //navigation bar button functions
 function showCodingNav() {
-  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><span class="dropContentSpan"><button class="button" onclick="showPens(document.getElementById(\'imgContainer\'))">Codepens</button><button class="button" onclick="showLayouts(document.getElementById(\'show-area\'))">Layouts</button></span</div>'
+  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><span class="dropContentSpan"><button class="button" onclick="showPens(document.getElementById(\'imgContainer\'))">Codepens</button></span></div>'
 }
 function showGraphicsNav() {
-  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><span class="dropContentSpan"><button class="button" onclick="showGraphics(document.getElementById(\'imgContainer\'))">Graphics</button></span></div>'
+  document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><span class="dropContentSpan"><button class="button" onclick="showGraphics(document.getElementById(\'imgContainer\'))">LANDSCAPES</button>  <button class="button" onclick="showGraphics(document.getElementById(\'imgContainer\'))">PORTRAITS</button>  <button class="button" onclick="showGraphics(document.getElementById(\'imgContainer\'))">CREATURES</button></span></div>'
 }
 function showProgramsNav() {
   document.getElementById('dropDownContent').innerHTML = '<div class="dropDownContent"><span class="dropContentSpan"><button class="button" onclick="showSoftware(document.getElementById(\'imgContainer\'))">Software</button><button class="button" onclick="showApps(document.getElementById(\'show-area\'))">Apps</button></span></div>'
 }
 //drop down menue content button functions
+//CODEPENS BUTTON 
 function showPens() {
   document.getElementById('imgContainer').innerHTML = '<div class="imgContainer"><span class="penButtonSpan"><button class="Button"><a href="https://codepen.io/DemonNoSh0kan/pen/zYJeWLz">Hover Dropdown Menu</a></button><button class="Button"><a href="https://codepen.io/DemonNoSh0kan/pen/vYQmRwG">Popup Onclick</a></button><button class="Button"><a href="https://codepen.io/DemonNoSh0kan/pen/VwVmbOe">Replace HTML Onclick</a></button></span><span class="penButtonSpan"><button class="Button"><a href="">Space Odyssey</a></button></span></div>'
 }
-function showLayouts() {
-  document.getElementById('imgContainer').innerHTML = '<div class="imgContainer"><span class="layoutsSpan"><button id="openFullView"><img src="img/layout001.jpg" alt="layout001" class="img"></button><div class="fullView"><button id=""exitFullView><h1>it works</h1></button></div></span></div>'
+//LANDSCAPES BUTTON
+function LANDSCAPES() {
+  document.getElementById('imgContainer').innerHTML = '<div class="imgContainer"></div>';
 }
-function showGraphics() {
-  document.getElementById('imgContainer').innerHTML = '<div class="imgContainer"><div class="item__container"><div class="item_img_container"></div></div></div>';
+//PORTRAITS BUTTON
+function PORTRAITS() {
+  document.getElementById('imgContainer').innerHTML ='<div class="imgContainer"></div>'
+}
+//CREATURES BUTTON
+function CREATURES() {
+  document.getElementById('imgContainer').innerHTML ='<div class="imgContainer"></div>'
 }
 function showSoftware() {
   document.getElementById('imgContainer').innerHTML = '<div class="imgContainer"><div class="item__container"><div class="item_img_container"></div></div></div>'
@@ -24,28 +31,4 @@ function showSoftware() {
 function showApps() {
   document.getElementById('imgContainer').innerHTML = '<div class="imgContainer"><div class="item__container"><div class="item_img_container"></div></div></div>'
 }
-//layout pop up funtions
-function closeForm() {
-  $('.fullView').removeClass('isVisible');
-}
 
-$(document).ready(function($) {
-  
-  /* Contact Form Interactions */
-  $('#openFullView').on('click', function(event) {
-    event.preventDefault();
-
-    $('.fullView').addClass('isVisible');
-  });
-  
-    //close popup when clicking x or off popup
-  $('.fullView').on('click', function(event) {
-    if ($(event.target).is('.fullView') || $(event.target).is('#exitFullView')) {
-      event.preventDefault();
-      $(this).removeClass('isVisible');
-    }
-  });
-  
-  
-  
-  });
